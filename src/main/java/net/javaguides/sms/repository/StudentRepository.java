a644b94d0b816cb32676b70b2cbc107e48ab250b
Gilbert Ngeno <ngenogilbert07@gmail.com>
2021-10-21 11:39:33 +0300

    Connected to MySQL

---
+++ src/main/java/net/javaguides/sms/repository/StudentRepository.java
@@ -0,0 +1,16 @@
+package net.javaguides.sms.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguides.sms.entity.Student;
+
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+	List<Student> saveAll(Object student);
+	
+	
+
+}
